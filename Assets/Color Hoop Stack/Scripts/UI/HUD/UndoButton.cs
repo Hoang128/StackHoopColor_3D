5f49c0adc1a5a415e679724bf8cf5b7415759bc2
Hoang128 <hoang1281998@gmail.com>
2021-10-04 17:18:15 +0700

    + Added number counter at undo button

--- Assets/Color Hoop Stack/Scripts/UI/HUD/UndoButton.cs
+++ Assets/Color Hoop Stack/Scripts/UI/HUD/UndoButton.cs
@@ -1,15 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UndoButton : MonoBehaviour
 {
+    [HideInInspector]
+    private int UndoTime = 5;
+    public TextMeshProUGUI undoNumberText;
+
     public void UndoMove()
     {
         if (GameplayMgr.Instance.stateMachine.CurrentState == GameplayMgr.Instance.stateGameplayIdle)
         {
-            InputMgr.Instance.UndoMove();
-            SoundsMgr.Instance.PlaySFX(SoundsMgr.Instance.sfxListConfig.sfxConfigDic[SFXType.BUTTON], false);
+            if (InputMgr.Instance.moveStack.Count > 0)
+            {
+                HandleUndoNumber();
+
+                undoNumberText.text = "<sprite index=" + UndoTime + ">";
+                InputMgr.Instance.UndoMove();
+                SoundsMgr.Instance.PlaySFX(SoundsMgr.Instance.sfxListConfig.sfxConfigDic[SFXType.BUTTON], false);
+            }
+        }
+    }
+
+    public void HandleUndoNumber()
+    {
+        if (UndoTime > 0)
+        {
+            if (UndoTime == 0)
+            {
+                UndoTime = 5;
+                return;
+            }
+            UndoTime--;
+        }
+        else
+        {
+            UndoTime = 5;
         }
     }
 }
